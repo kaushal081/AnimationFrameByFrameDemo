43da17f86611ad0abef80fa0aaf93cf3c5a1e379
kaushal <kaushal.comp@gmail.com>
2018-07-01 17:41:58 +0530

    Animation

---
+++ app/src/main/java/com/example/kaushal/animationframebyframedemo/MainActivity.java
@@ -0,0 +1,62 @@
+
+package com.example.kaushal.animationframebyframedemo;
+
+import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+	AnimationDrawable mframeAnimation = null;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.framebyframe);
+		
+	     // Handle Start Button
+		final Button onButton = (Button) findViewById(R.id.ButtonStart);
+		onButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				ImageView img = (ImageView)findViewById(R.id.ImageView_Juggle);
+
+				BitmapDrawable frame1 = (BitmapDrawable)getResources().
+						getDrawable(R.drawable.splash1);
+				BitmapDrawable frame2 = (BitmapDrawable)getResources().
+						getDrawable(R.drawable.splash2);
+				BitmapDrawable frame3 = (BitmapDrawable)getResources().
+						getDrawable(R.drawable.splash3);
+
+				// Get the background, which has been compiled to an AnimationDrawable object.
+				int duration = 250;
+				mframeAnimation = new AnimationDrawable();
+				mframeAnimation.setOneShot(false);	// loop continuously
+				mframeAnimation.addFrame(frame1, duration);
+				mframeAnimation.addFrame(frame2, duration);
+				mframeAnimation.addFrame(frame3, duration);
+
+				img.setBackgroundDrawable(mframeAnimation);
+
+
+				mframeAnimation.start();
+
+			}
+		});
+			
+		// Handle Stop Button
+		final Button offButton = (Button) findViewById(R.id.ButtonStop);
+		offButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				mframeAnimation.stop();
+
+			}
+		});
+	    
+	}
+
+	
+
+}
